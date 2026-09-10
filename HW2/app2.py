8f1f0decf6a06f37680f0a1a3514254fa7576ff6
SophiaD <79029238+flandrelll@users.noreply.github.com>
2026-09-09 21:38:36 -0400

    Update app2.py

--- HW2/app2.py
+++ HW2/app2.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-st.title("Data Visualization of Baseball Statistics")
+st.title("HW2 - Data Visualization of Baseball Statistics")
 
 df = pd.read_csv("HW2/BaseballHeightWeight.csv")
 
